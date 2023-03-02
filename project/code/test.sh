3666898046799ba9730d0995939173819ebfa0f9
IvanaXu <1440420407@qq.com>
2023-03-02 09:09:54 +0800

    update

--- project/code/test.sh
+++
@@ -1,2 +0,0 @@
-batch=100
-python test/predict.py --device gpu --params_path ../user_data/model_data/model_state.pdparams --batch_size $batch
--- project/code/train.sh
+++
@@ -1,5 +0,0 @@
-
-python train/cdata.py
-
-batch=100
-python -m paddle.distributed.launch --gpus "0" train/train.py --device gpu --save_dir ../user_data --epochs 24 --batch_size $batch
