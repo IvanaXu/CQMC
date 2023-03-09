d485ef45ee6f91b31170353508687b387524c1fe
IvanaXu <1440420407@qq.com>
2023-03-09 11:08:17 +0800

    #7 Epoch:12 /0.7884 ~

--- README.md.py
+++ README.md.py
@@ -68,7 +68,7 @@ with open("README.md", "w") as f:
 #
 p1 = "#7"
 p2 = "Epoch:12"
-os.system(f"""git add * && git commit -m "{p1} {p2} /{score:.4f}" && git push""")
+os.system(f"""git add * && git commit -m "{p1} {p2} /{score:.4f} ~" && git push""")
         
 
 
