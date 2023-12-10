b1ee65ce274e3588c48bd356221e7ae27e93d1ad
IvanaXu <1440420407@qq.com>
2023-12-10 21:42:53 +0800

    #21 erniebot/ERROR erniebot.errors.InvalidArgumentError: `input` has too many elements.

--- erniebot/APP001/02.getEMB.py
+++ erniebot/APP001/02.getEMB.py
@@ -6,37 +6,72 @@ from config import KEY
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
+
 #
 erniebot.api_type = 'aistudio'
 erniebot.access_token = KEY
 print(f"KEY {KEY}")
 
+
 #
-trainE1 = pd.read_csv("../user_data/cut_data/trainE1.csv", sep="\t", header=None)
-trainE2 = pd.read_csv("../user_data/cut_data/trainE2.csv", sep="\t", header=None)
-print(trainE1.shape, trainE2.shape)
+L, N = [], 10
 
-_, trainE1 = train_test_split(trainE1, test_size=5)
-_, trainE2 = train_test_split(trainE2, test_size=5)
-print(trainE1.shape, trainE2.shape)
+trainE1 = pd.read_csv("../user_data/cut_data/trainE1.csv", sep="\t", header=None).head(N)
+trainE1["T"] = "trainE1"
+L.append(trainE1)
+
+trainE2 = pd.read_csv("../user_data/cut_data/trainE2.csv", sep="\t", header=None).head(N)
+trainE2["T"] = "trainE2"
+L.append(trainE2)
+
+test3 = pd.read_csv("../xfdata/3/test.tsv", sep="\t", header=None).head(N)
+test3[2] = -1
+test3["T"] = "test3"
+L.append(test3)
+
+test5 = pd.read_csv("../xfdata/5/test.csv", sep="\t", header=None).head(N)
+test5[2] = -1
+test5["T"] = "test5"
+L.append(test5)
+
+for task in ["bq_corpus", "lcqmc", "paws-x-zh"]:
+    test6 = pd.read_csv(f"../xfdata/6/{task}/test.tsv", sep="\t", header=None).head(N)
+    test6[2] = -1
+    test6["T"] = f"test6_{task}"
+    L.append(test6)
+
+data = pd.concat(L)
+print(pd.value_counts(data["T"]))
 
 
-def get_Embedding(df, name):
-    _X, _Y = [], []
-    for x1, x2, y in tqdm(zip(df[0], df[1], df[2]), total=len(df)):
-        response = erniebot.Embedding.create(
-            model='ernie-text-embedding',
-            input=[x1, x2])
-
-        _X.append(np.concatenate(response.get_result(), axis=0))
+def get_Embedding(df):
+    _X, _Y, _T = [], [], []
+    for x1, x2, y, t in tqdm(zip(df[0], df[1], df[2], df["T"]), total=len(df)):
+        _X.append(f"{x1}\t{x2}")
         _Y.append(y)
-        break
+        _T.append(t)
 
-    data = pd.DataFrame(_X)
+    response = erniebot.Embedding.create(
+        model='ernie-text-embedding',
+        input=_X,
+    )
+
+    data = pd.DataFrame(response.get_result())
     data["Y"] = _Y
-    data.to_csv(f"../user_data/cut_data/{name}_EMB.csv")
+    data["T"] = _T
+
+    #
+    for _type in [
+        "trainE1",
+        "trainE2",
+        "test3",
+        "test5",
+        "test6_bq_corpus", "test6_lcqmc", "test6_paws-x-zh"
+    ]:
+        data[data["T"] == _type].to_csv(f"../user_data/cut_data/{_type}_EMB.csv", index=False)
 
 
-# get_Embedding(df=trainE1, name="trainE1")
-get_Embedding(df=trainE2, name="trainE2")
+get_Embedding(df=data)
+
+
 
