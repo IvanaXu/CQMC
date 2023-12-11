72bfbd559c03602bbd3a8fd8a6a05738628a0d8c
IvanaXu <1440420407@qq.com>
2023-12-11 22:20:38 +0800

    #21 erniebot Fixed.

--- erniebot/APP001/02.getEMB.py
+++ erniebot/APP001/02.getEMB.py
@@ -1,4 +1,7 @@
 #
+import os
+import time
+import pickle
 import erniebot
 import numpy as np
 import pandas as pd
@@ -14,31 +17,38 @@ print(f"KEY {KEY}")
 
 
 #
-L, N = [], 10
+L, N = [], None
 
-trainE1 = pd.read_csv("../user_data/cut_data/trainE1.csv", sep="\t", header=None).head(N)
+trainE1 = pd.read_csv("../user_data/cut_data/trainE1.csv", sep="\t", header=None)
+print(f"trainE1 {trainE1.shape}")
 trainE1["T"] = "trainE1"
+_, trainE1 = train_test_split(trainE1, test_size=2000, random_state=10086)
 L.append(trainE1)
 
-trainE2 = pd.read_csv("../user_data/cut_data/trainE2.csv", sep="\t", header=None).head(N)
+trainE2 = pd.read_csv("../user_data/cut_data/trainE2.csv", sep="\t", header=None)
+print(f"trainE2 {trainE2.shape}")
 trainE2["T"] = "trainE2"
+_, trainE2 = train_test_split(trainE2, test_size=2000, random_state=10086)
 L.append(trainE2)
 
-test3 = pd.read_csv("../xfdata/3/test.tsv", sep="\t", header=None).head(N)
+test3 = pd.read_csv("../xfdata/3/test.tsv", sep="\t", header=None)
+print(f"test3 {test3.shape}")
 test3[2] = -1
 test3["T"] = "test3"
 L.append(test3)
 
-test5 = pd.read_csv("../xfdata/5/test.csv", sep="\t", header=None).head(N)
+test5 = pd.read_csv("../xfdata/5/test.csv", sep="\t", header=None)
+print(f"test5 {test5.shape}")
 test5[2] = -1
 test5["T"] = "test5"
-L.append(test5)
+# L.append(test5)
 
 for task in ["bq_corpus", "lcqmc", "paws-x-zh"]:
-    test6 = pd.read_csv(f"../xfdata/6/{task}/test.tsv", sep="\t", header=None).head(N)
+    test6 = pd.read_csv(f"../xfdata/6/{task}/test.tsv", sep="\t", header=None)
+    print(f"test6 {test6.shape}")
     test6[2] = -1
     test6["T"] = f"test6_{task}"
-    L.append(test6)
+    # L.append(test6)
 
 data = pd.concat(L)
 print(pd.value_counts(data["T"]))
@@ -47,14 +57,31 @@ print(pd.value_counts(data["T"]))
 def get_Embedding(df):
     _X, _Y, _T = [], [], []
     for x1, x2, y, t in tqdm(zip(df[0], df[1], df[2], df["T"]), total=len(df)):
-        _X.append(f"""这句话'{x1}' 和 这句话'{x2}' 是一个意思么?""")
+        _X.append([x1, x2])
         _Y.append(y)
         _T.append(t)
 
-    data = pd.DataFrame([erniebot.ChatCompletion.create(
-        model="ernie-bot-turbo",
-        messages=[{'role': 'user', 'content': _x}],
-    ).get_result() for _x in tqdm(_X, total=len(_X))])
+    data = []
+    for _n, _x in tqdm(enumerate(_X), total=len(_X)):
+        _temp = f"/Volumes/ESSD/TEMP/{_n:09d}"
+
+        if not os.path.exists(_temp):
+            Embedding = erniebot.Embedding.create(
+                model='ernie-text-embedding',
+                input=_x
+            ).get_result()
+            time.sleep(1)
+
+            EMB = np.concatenate(Embedding, axis=0)
+            with open(_temp, "wb") as f:
+                pickle.dump(EMB, f)
+        else:
+            with open(_temp, "rb") as f:
+                EMB = pickle.load(f)
+        #
+        data.append(EMB)
+
+    data = pd.DataFrame(data)
     data["Y"] = _Y
     data["T"] = _T
 
@@ -63,8 +90,8 @@ def get_Embedding(df):
         "trainE1",
         "trainE2",
         "test3",
-        "test5",
-        "test6_bq_corpus", "test6_lcqmc", "test6_paws-x-zh"
+        # "test5",
+        # "test6_bq_corpus", "test6_lcqmc", "test6_paws-x-zh",
     ]:
         data[data["T"] == _type].to_csv(f"../user_data/cut_data/{_type}_EMB.csv", index=False)
 
