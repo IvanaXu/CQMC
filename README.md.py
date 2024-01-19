88c1f0e85cd090e7ea6d8b5f8f44c09ce144635b
IvanaXu <1440420407@qq.com>
2024-01-19 23:51:27 +0800

    #12 Erniebot /-0.6189 version

--- README.md.py
+++ README.md.py
@@ -8,12 +8,12 @@ p2 = "Erniebot"
 scoreL = """
 -1
 -1
-0.7671
-0.7359	
-0.6828	
-0.5455
+0.7192
+0.7613
+0.6737
+0.5405
 """
-test = "ACC: -1 F1: 0.6808"
+test = "ACC: -1 F1: 0.7127"
 
 scoreL = [float(i) for i in scoreL.split("\n") if i]
 score = (scoreL[0]/0.6365 + scoreL[1]/0.9998 + scoreL[4]/0.9428)/3.0
